5153de997012568edaf11fc9c0d5aaedb38ce0f6
vimtoo <vimtoocom@gmail.com>
2019-02-11 16:48:38 +0800

    删除对python3的支持 导致 ycm 中报错

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ yum update -y
 
 
 yum install -y git gcc-c++ ncurses-devel python-devel cmake wget make
-yum install -y python36 python36-devel
 
 
 cd ~
@@ -18,9 +17,7 @@ cd vim-8.1.0000/
 
 ./configure --prefix=/usr/local/vim  \
 --enable-pythoninterp=yes \
---enable-python3interp=yes \
 --with-python-command=python \
---with-python3-command=python36
 
 make && make install
 
